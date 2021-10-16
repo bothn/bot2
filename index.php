413b1e19effa47c14cba41b619f412a8b1403050
bothn <92487044+bothn@users.noreply.github.com>
2021-10-16 16:05:55 -0600

    Update index.php

--- index.php
+++ index.php
@@ -1,9 +1,10 @@
 <?php
 $weather  = json_decode(file_get_contents ("http://190.4.63.192/reportes/wcdma/reports_kpis_h/test3json.php"),true);
   
- 
+
+
   foreach ($weather as $pc) { 
-    $a[]= $pc["FECHA_HXH"]     ;
+    $a[]= $pc[FECHA_HXH]     ;
     $b[]= $pc["SUCCESS"]     ;
  
 }
