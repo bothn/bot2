b5cd0494e1e05d71e2eb7dc83028e357208440c1
bothn <92487044+bothn@users.noreply.github.com>
2021-10-16 16:32:02 -0600

    Update index.php

--- index.php
+++ index.php
@@ -10,6 +10,40 @@ $weather  = json_decode(file_get_contents ("http://190.4.63.192/reportes/wcdma/r
 }
  
 $final_key = '['.implode(",", $a).']';
- echo $final_key;
-  
+
+  $chartConfig = '{
+  "type": "line",
+  "data": {
+    "labels": $final_key,
+    "datasets": [{
+      "label": "Users",
+      "data": [' . implode(',', $data) . ']
+    }]
+  },,
+  "options": {
+    "title": {
+      "text": "Chart.js Time Scale"
+    },
+    "scales": {
+      "xAxes": [{
+        "type": "time",
+        "time": {
+          "parser": "MM/DD/YYYY HH:mm",
+        },
+        "scaleLabel": {
+          "display": true,
+          "labelString": "Date"
+        }
+      }],
+      "yAxes": [{
+        "scaleLabel": {
+          "display": true,
+          "labelString": "value"
+        }
+      }]
+    }
+  }
+}';
+
+ echo $chartConfig;
 ?> 
