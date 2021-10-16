e870135d64c25fccd743a981e8b98db46c1dfd99
bothn <92487044+bothn@users.noreply.github.com>
2021-10-16 01:01:35 -0600

    Update index.php

--- index.php
+++ index.php
@@ -1,15 +1,9 @@
 <?php
 $pc = json_decode(file_get_contents ("http://190.4.63.192/reportes/wcdma/reports_kpis_h/test3json.php"),true);
  
-$data = $pc["SUCCESS"];
-foreach ($weather as $pc) { 
-    $FECHA_HXH= $pc["FECHA_HXH"]." ";
-    $INTENTOS= $pc["INTENTOS"]." ";
-    $SUCCESS= $pc["SUCCESS"]." ";
  
-}
-
+ $a1 = $pc["FECHA_HXH"]);
  
-echo [' . implode(',',$pc["FECHA_HXH"]) . '];
+echo implode("','",$a1 ) ;
 echo $chartUrl = 'https://quickchart.io/chart?w=500&h=300&c=' . urlencode($chartConfig);
 ?>
