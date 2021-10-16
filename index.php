f9f0f7bbc19570d6a3184f5e4a5db204265a70fb
bothn <92487044+bothn@users.noreply.github.com>
2021-10-16 15:21:18 -0600

    Update index.php

--- index.php
+++ index.php
@@ -18,7 +18,7 @@ $xc = $a;
 $chartConfigArr = array(
   'type' => 'bar',
   'data' => array(
-    'labels' => $dates,
+    'labels' => [$dates],
     'datasets' => array(
       array(
         'label' => 'Users',
