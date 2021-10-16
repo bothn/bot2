72d2bd3f5d8b2de44813555a07532d8b52d547c2
bothn <92487044+bothn@users.noreply.github.com>
2021-10-16 01:48:15 -0600

    Update index.php

--- index.php
+++ index.php
@@ -18,34 +18,7 @@ $chartConfig = '{
       "label": "Users",
       "data": '.$exitosas.'
     }]
-  },"options" : {
- 
- 
- 
-	"scales": {
-		"type": "time",
-		"xAxes": [  {
-            "type": "time", 
-			"time": {
-                "displayFormats": {
-					"millisecond": "YYYY-MM DD HH",
-					"second": "YYYY-MM DD HH",
-					"minute": "YYYY-MM DD HH",
-					"hour": "YYYY-MM DD HH",
-					"day": "YYYY-MM DD HH",
-					"week": "YYYY-MM DD HH",
-					"month": "YYYY-MM DD HH",
-					"quarter": "YYYY-MM DD HH",
-					"year": "YYYY-MM DD HH",
-								 }
-                    
-                } 
-            }] 
-			  
-	}
-
-	
-	}
+  }
   
 }';
 
