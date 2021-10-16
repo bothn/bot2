245fdeb612160d22e18f7f9f2cc50f17b21b2a00
bothn <92487044+bothn@users.noreply.github.com>
2021-10-16 01:44:09 -0600

    Update index.php

--- index.php
+++ index.php
@@ -11,14 +11,42 @@ $weather  = json_decode(file_get_contents ("http://190.4.63.192/reportes/wcdma/r
  $exitosas= implode(",", $b);
 
 $chartConfig = '{
-  "type": "bar",
+  "type": "line",
   "data": {
     "labels":'. $fecha.',
     "datasets": [{
       "label": "Users",
       "data": '.$exitosas.'
     }]
-  }
+  },"options" : {
+ 
+ 
+ 
+	scales: {
+		type: 'time',
+		xAxes: [  {
+            type: 'time', 
+			time: {
+                displayFormats: {
+					'millisecond': 'YYYY-MM DD HH',
+					'second': 'YYYY-MM DD HH',
+					'minute': 'YYYY-MM DD HH',
+					'hour': 'YYYY-MM DD HH',
+					'day': 'YYYY-MM DD HH',
+					'week': 'YYYY-MM DD HH',
+					'month': 'YYYY-MM DD HH',
+					'quarter': 'YYYY-MM DD HH',
+					'year': 'YYYY-MM DD HH',
+								 }
+                    
+                } 
+            }] 
+			  
+	}
+
+	
+	}
+  
 }';
 
 echo $chartUrl = 'https://quickchart.io/chart?w=500&h=300&c=' . urlencode($chartConfig);
