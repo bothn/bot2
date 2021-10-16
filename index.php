ea464fa785825f42c9f21a7d39fe3c40805cf505
bothn <92487044+bothn@users.noreply.github.com>
2021-10-16 01:08:15 -0600

    Update index.php

--- index.php
+++ index.php
@@ -2,6 +2,6 @@
 $pc = json_decode(file_get_contents ("http://190.4.63.192/reportes/wcdma/reports_kpis_h/test3json.php"),true);
  
  
- echo implode(", ", array_map(function($obj) { foreach ($obj as $p => $v) { return $p;} }, $pc));
+ echo implode(", ", array_map(function($obj) { foreach ($obj as $p => $v) { return $v;} }, $pc));
 echo $chartUrl = 'https://quickchart.io/chart?w=500&h=300&c=' . urlencode($chartConfig);
 ?>
