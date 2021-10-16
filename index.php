af4d4fe3bfd2ea00d230be18fda7508e1b49ebc0
bothn <92487044+bothn@users.noreply.github.com>
2021-10-16 08:29:19 -0600

    Update index.php

--- index.php
+++ index.php
@@ -23,7 +23,7 @@ $chartConfig = '{
     "scales": {
       "xAxes": [{
         "type": "time",
-        "time": {"parser": "M/D/YYYY HH:mm:ss"},
+        "time": {"parser": "MM/DD/YYYY HH:mm:ss"},
         "scaleLabel": {"display": true, "labelString": "Date"}
       }]
     }
