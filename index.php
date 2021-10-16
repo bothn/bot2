17abec66016c577f906abeed47f957454ac0da3f
bothn <92487044+bothn@users.noreply.github.com>
2021-10-16 01:06:42 -0600

    Update index.php

--- index.php
+++ index.php
@@ -1,11 +1,7 @@
 <?php
 $pc = json_decode(file_get_contents ("http://190.4.63.192/reportes/wcdma/reports_kpis_h/test3json.php"),true);
- $id_nums = array(1,6,12,18,24);
-
-$id_nums = implode(", ", $id_nums);
  
- $a1 = $pc["FECHA_HXH"];
  
-echo implode("','",$id_nums ) ;
+ echo implode(", ", array_map(function($obj) { foreach ($obj as $p => $v) { return $p;} }, $pc));
 echo $chartUrl = 'https://quickchart.io/chart?w=500&h=300&c=' . urlencode($chartConfig);
 ?>
