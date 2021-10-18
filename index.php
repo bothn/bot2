d8744b3d82050ef287cd57c4da711ca978f45f84
bothn <92487044+bothn@users.noreply.github.com>
2021-10-17 19:32:10 -0600

    Update index.php

--- index.php
+++ index.php
@@ -39,12 +39,12 @@ $chartConfig = '{
   }
 } ';
 $Config = '{"chart":{"type":"line","data":{"labels":["'.implode('","', $a).'"],"datasets":[{"label":"Time series example","fill":false,"data":["'.implode('","', $b).'"],}]},"options":{"scales":{"xAxes":[{"type":"time","time":{"format":"HH:mm","unit":"hour","unitStepSize":300,"displayFormats":{"minute":"HH:mm","hour":"HH:mm","min":"00:00","max":"23:59"}}}]}}}}';
-$post_data = json_encode($chartConfig, JSON_UNESCAPED_SLASHES);
+$post_data = json_encode($Config, JSON_UNESCAPED_SLASHES);
  $x=$config;
  $test='{"chart": {"type": "bar", "data": {"labels": ["Hello", "World"], "datasets": [{"label": "Foo", "data": ["1", "2"]}]}}}';
 $ch = curl_init('https://quickchart.io/chart/create');
 curl_setopt($ch, CURLOPT_POST, true);
-curl_setopt($ch, CURLOPT_POSTFIELDS, $x);
+curl_setopt($ch, CURLOPT_POSTFIELDS, $post_data);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true); 
 curl_setopt($ch, CURLOPT_HTTPHEADER, array("Content-Type: application/json")); 
 $response = curl_exec($ch);
