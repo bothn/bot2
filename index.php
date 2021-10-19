0b27c044069bec8b222333a1fdc1737a5fd9c14c
bothn <92487044+bothn@users.noreply.github.com>
2021-10-19 15:52:35 -0600

    Update index.php

--- index.php
+++ index.php
@@ -10,51 +10,9 @@ $weather  = json_decode(file_get_contents ("http://190.4.63.192/reportes/wcdma/r
  
 }
  
-$chartConfig = '{
-  "type": "line",
-  "data": {
-    "datasets": [
-          {
-        "label": "Time series example",
-        "fill": false,
-        "data": [' . implode(',', $a) . '],
-        },
-        {
-        "label": "segundo",
-        "fill": false,
-        "data": [' . implode(',', $b) . '],
-        }
-    
-    ]
-  },  "options": {
-    "scales": {
-      "xAxes":[{
-  "type": "time",
-  "time": {
-    "format": "HH:mm",
-    "unit": "minute",
-    "unitStepSize": 300,
-    "displayFormats": {
-      "minute": "HH:mm", 
-      "hour": "HH:mm", 
-      "min": "00:00",
-      "max": "23:59"
-    },
-}}],
-    },
-  }
-} ';
+$chartConfig = implode(',', $b) ;
 
-$post_data = json_encode($chartConfig, JSON_UNESCAPED_SLASHES);
  
- $test='{"chart": {"type": "bar", "data": {"labels": ["Hello", "World"], "datasets": [{"label": "Foo", "data": ["1", "2"]}]}}}';
-$ch = curl_init('https://quickchart.io/chart/create');
-curl_setopt($ch, CURLOPT_POST, true);
-curl_setopt($ch, CURLOPT_POSTFIELDS, $post_data);
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, true); 
-curl_setopt($ch, CURLOPT_HTTPHEADER, array("Content-Type: application/json")); 
-$response = curl_exec($ch);
-print_r($response);
-
+ 
 echo $chartConfig ;
 ?> 
