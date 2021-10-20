884b99f1a15c5a6bff8c7fa3d6c6b28a7acb6c72
bothn <92487044+bothn@users.noreply.github.com>
2021-10-20 10:38:21 -0600

    Update index.php

--- index.php
+++ index.php
@@ -67,10 +67,10 @@ $response = curl_exec($ch);
 $grafica = json_decode($response);
  
 
-$grafica2= $grafica->url; 
-$path = "https://api.telegram.org/bot1981081794:AAG3LTrHyuTEAk3bcoSAcATP7CJU3nBWyP0";
+$grafica2=$grafica->url; 
+$path="https://api.telegram.org/bot1981081794:AAG3LTrHyuTEAk3bcoSAcATP7CJU3nBWyP0";
 $chat_id="-599009724";
- $photo=$grafica2;
+$photo=$grafica2;
 echo $chat_id;
 echo $path;
 
