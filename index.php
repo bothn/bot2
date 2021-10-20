9fd58e95f8456f534eb9412e16313a395a41e23c
bothn <92487044+bothn@users.noreply.github.com>
2021-10-20 11:03:11 -0600

    Update index.php

--- index.php
+++ index.php
@@ -72,6 +72,6 @@ $path="https://api.telegram.org/bot2069143370:AAGiUdwcQETanfLBHtZ32S621Hpqwp8sT8
 $chat_id="-599009724";
  $xc=$grafica->url;
  
-file_get_contents($path."/sendPhoto?chat_id=-599009724&photo=".$xc);
+file_get_contents($path."/sendPhoto?chat_id=".$chat_id."&photo=".$xc);
  
 ?> 
