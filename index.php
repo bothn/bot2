b7bb108117a3508d1b65ae210cf9aa47def359f8
bothn <92487044+bothn@users.noreply.github.com>
2021-10-19 23:20:16 -0600

    Update index.php

--- index.php
+++ index.php
@@ -38,6 +38,14 @@ $chartConfig = '{"width":900,"height":200, "chart":{
 
     ] },
   "options": {    
+      "plugins": {
+      "legend": {
+        "position": "top",
+      },
+      "title": {
+        "display": true,
+        "text": "Chart.js Bar Chart"
+      },
     "scales": {
       "xAxes": [{
         "type": "time",
