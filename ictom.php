9ad0d5142b8d1ebe3df8fe81b4b77491ff58cb23
bothn <92487044+bothn@users.noreply.github.com>
2022-08-20 08:26:09 -0600

    Update ictom.php

--- ictom.php
+++ ictom.php
@@ -1,6 +1,6 @@
 <?php
 // nos  conectamos a ejemplo.com y al puerto 3307
-$enlace = mysql_connect('sql.freedb.tech:3306',  'freedb_srvsusehn1', '*J*eskg%*6sSfcB');
+$enlace = mysql_connect('sql.freedb.tech',  'freedb_srvsusehn1', '*J*eskg%*6sSfcB');
 if  (!$enlace) {
     die('No pudo conectarse: ' . mysql_error());
 }
