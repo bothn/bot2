98b0fe25db8c40a4cf63c3d7cdfdedffddc4af24
bothn <92487044+bothn@users.noreply.github.com>
2022-08-22 07:07:52 -0600

    Update ictom.php

--- ictom.php
+++ ictom.php
@@ -84,7 +84,7 @@ if ($tt=="" && $operacion == "SDMAPERTURA"){
 	//	$alarma="CELDA FUERA DE SERVICIO";
 	//}
 	$main_taoli1="INSERT INTO markers(alarma,name,address,lat,lng,type,hora_oos,tiempo,fecha_insert,territorio,ne_sitename,morfo,tt,avance) VALUES ('$alarma','$site','$sitename2','$lat','$lon','$tec','$fei','$tiempo','$today','$terr','$sitename','$mor','$id','$desc')";
-	 $conn_taoli -> query($main_taoli1) or die(mysql_error());
+	 $conn_taoli -> query($main_taoli1) ;
  
 	
 	
