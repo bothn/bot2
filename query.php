d180bb84215374c088c85e0e580920c8542ba109
bothn <92487044+bothn@users.noreply.github.com>
2022-08-22 14:50:03 -0600

    Update query.php

--- query.php
+++ query.php
@@ -42,36 +42,26 @@ $main_taoli=$conn_taoli -> query("select * FROM markers WHERE tt='".$id."'");
 $row_main_taoli_result=mysqli_fetch_assoc($main_taoli);
 $tt=$row_main_taoli_result['tt'];
  
-	$site='sdf';
-	$sitename='1d';
-	$sitename = str_replace("+", " ",$sitename);
-	$lat='-12';
-	$lon='87';
-	$tec='4G';
-	$mor=$array2[5];
-	$fei='';
- 
-	 
-	$desc="asdasdasd";
+ $alarma="asdas";
+ $site="asdasd";
+ $sitename2='dfsdf'
+ $lat="";
+ $lon"";
+ $tec"";
+ $fei"";
+ $tiempo"12";
+ $today"";
+ $terr"";
+ $sitename"";
+ $mor"";
+ $id"";
+ $desc"";
   
-	//$terr = substr($sitename,strpos($sitename,"(")+1,2);
-	$terr = 'jj';
-	//$halt = $array2[9];
-	$sitename2="[".substr($sitename,0,strpos($sitename," "))."]";
-	$fei = date('Y-m-d H:i:s',$fei);
-	$alarma="CELDA FUERA DE SERVICIO";
-	
-	$fera= date('Ymd');
-	$hoira = date('H');
+ 
 
-	//if ($halt=="Yes"){
-	//	$alarma= "CELDA EN ESTADO HALTED";
-	//}else{
-	//	$alarma="CELDA FUERA DE SERVICIO";
-	//}
+ 
 	$main_taoli1="INSERT INTO markers(alarma,name,address,lat,lng,type,hora_oos,tiempo,fecha_insert,territorio,ne_sitename,morfo,tt,avance) VALUES ('$alarma','$site','$sitename2','$lat','$lon','$tec','$fei','$tiempo','$today','$terr','$sitename','$mor','$id','$desc')";
-  //$conn_taoli -> query($main_taoli1) or die(mysql_error());
-  /// mysqli_query($conn_taoli, $main_taoli1);
+ 
 	
 		 if (mysqli_query($conn_taoli, $main_taoli1)) {
 		echo "New record created successfully !";
