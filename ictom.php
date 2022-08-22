9549b43809a5fc9fd9a507862fca509789581af2
bothn <92487044+bothn@users.noreply.github.com>
2022-08-22 00:26:45 -0600

    Update ictom.php

--- ictom.php
+++ ictom.php
@@ -10,7 +10,7 @@ $conn_taoli = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
 mysqli_set_charset($con, "utf8");
  
  
-<?php
+
 //$URL_K = "http://192.168.127.132:13013/sendsms?username=foo&password=bar&from=99999999&to=50495298662&text=textoprueba";
 //$FA_TT = fopen($URL_K,"r");
 
@@ -73,89 +73,8 @@ $nombre_contacto='';
 $desicion_tyc='';
 $desicion_ss='';
 	
-$main_taoli=mysql_query("select * FROM markers WHERE tt='".$id."'",$conn_taoli);
-$row_main_taoli_result=mysql_fetch_assoc($main_taoli);
-$tt=$row_main_taoli_result['tt'];
-	
-if ($tt=="" && $operacion == "SDMAPERTURA"){
-	
-	$buscar=trim($buscar);
-	$array2=explode("*",$buscar);
-	$site=$array2[0];
-	$sitename=$array2[1];
-	$sitename = str_replace("+", " ",$sitename);
-	$lat=$array2[2];
-	$lon=$array2[3];
-	$tec=$array2[4];
-	$mor=$array2[5];
-	$fei=$array2[6];
-	$fei = str_replace("+", " ",$fei);
-	$fei=strtotime($fei);
-	$desc=$array2[7];
-	$desc = str_replace("+", " ",$desc);
-	$tiempo = (strtotime($today)-$fei);
-	//$terr = substr($sitename,strpos($sitename,"(")+1,2);
-	$terr = $array2[8];
-	//$halt = $array2[9];
-	$sitename2="[".substr($sitename,0,strpos($sitename," "))."]";
-	$fei = date('Y-m-d H:i:s',$fei);
-	$alarma="CELDA FUERA DE SERVICIO";
-	
-	$fera= date('Ymd');
-	$hoira = date('H');
-
-	//if ($halt=="Yes"){
-	//	$alarma= "CELDA EN ESTADO HALTED";
-	//}else{
-	//	$alarma="CELDA FUERA DE SERVICIO";
-	//}
-	$main_taoli="INSERT INTO markers(alarma,name,address,lat,lng,type,hora_oos,tiempo,fecha_insert,territorio,ne_sitename,morfo,tt,avance) VALUES ('$alarma','$site','$sitename2','$lat','$lon','$tec','$fei','$tiempo','$today','$terr','$sitename','$mor','$id','$desc')";
-	mysql_query($main_taoli,$conn_taoli) or die(mysql_error());
-	//$main_ra="INSERT INTO ra2 (ne_site,men_taoli_fecha,men_taoli_horai,tt) VALUES ('$site','$fera','$hoira','$id')";
-	//mysql_query($main_ra,$conn_ra) or die(mysql_error());
-	//$URL_K = "http://192.168.127.132:13013/sendsms?username=foo&password=bar&from=99999999&to=50495298662&text=apertura,$site,$fera,$hoira,$id";
-	//$FA_TT = fopen($URL_K,"r");
-	
-	
-}else if($tt!="" && $operacion == "SDMAVANCE"){
-	$buscar=trim($buscar);
-	$array2=explode("*",$buscar);
-	$fecha=$array2[0];
-	$fecha = str_replace("+", " ",$fecha);
-	$desc=$array2[1];
-	$desc = str_replace("+", " ",$desc);
-	$fei=$row_main_taoli_result['hora_oos'];
-	
-	///pendiente la hora del avance
-	$fei=strtotime($fei);
-	$tiempo = (strtotime($today) - $fei)/60/60;	
-	///fin pendiente
-	$halt = $array2[2];
-	$alarma="CELDA FUERA DE SERVICIO";
-	if ($halt=="Yes"){
-		$alarma = "CELDA EN ESTADO HALTED";
-	}else{
-		$alarma = "CELDA FUERA DE SERVICIO";
-	}
-		
-	$main_taoli="UPDATE markers SET fecha_avance='$fecha',tiempo='$tiempo',avance='$desc', alarma='$alarma' WHERE tt = '$id'";
-	
-	mysql_query($main_taoli,$conn_taoli) or die(mysql_error());
-	
-}else if ($tt!="" && $operacion == "SDMCIERRE"){
-		
-	$main_taoli="DELETE FROM markers WHERE tt = '$id'";
-	mysql_query($main_taoli,$conn_taoli) or die(mysql_error());
-	$hofra=date('H');
-	//$main_ra="UPDATE ra2 SET men_taoli_horaf = '$hofra' )";
-	//mysql_query($main_ra,$conn_ra) or die(mysql_error());
-	//$URL_K = "http://192.168.127.132:13013/sendsms?username=foo&password=bar&from=99999999&to=50495298662&text=cierre,$hofra,$id";
-	//$FA_TT = fopen($URL_K,"r");
-	
-	
-}
-
-
+$main_taoli=$conn_taoli -> query("select * FROM markers WHERE tt='".$id."'");
+ 
 ?>
 
  
