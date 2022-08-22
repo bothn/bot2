ce31accfa5bd78615a1b9b26d51373b31299b80f
bothn <92487044+bothn@users.noreply.github.com>
2022-08-22 11:54:37 -0600

    Update ictom.php

--- ictom.php
+++ ictom.php
@@ -78,7 +78,7 @@ if ($tt=="" && $operacion == "SDMAPERTURA"){
 	//}
 	$main_taoli1="INSERT INTO markers(alarma,name,address,lat,lng,type,hora_oos,tiempo,fecha_insert,territorio,ne_sitename,morfo,tt,avance) VALUES ('$alarma','$site','$sitename2','$lat','$lon','$tec','$fei','$tiempo','$today','$terr','$sitename','$mor','$id','$desc')";
 //$conn_taoli -> query($main_taoli1) or die(mysql_error());
- mysqli_query($conn_taoli, $main_taoli1)
+ mysqli_query($conn_taoli, $main_taoli1);
 	
 	
 }else if($tt!="" && $operacion == "SDMAVANCE"){
